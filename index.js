20a7dba951ab4b566184e8de2658eba85537c179
Joe Thor <joethor1989@gmail.com>
2018-07-29 20:12:22 -0500

    created and typed up readme. Removed the apikey from index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var oledExp = require("/usr/bin/node-oled-exp");
 oledExp.clear();
 oledExp.init();
  
-var APIkey = "33600f0073ced31aaa6969ba360fc0d0";
+var APIkey = "<APIKEY>";
 
 var lat = "lat=44.9778"
 var  long = "lon=-93.265"
@@ -27,13 +27,7 @@ https.get(queryURL, (resp) => {
     var sunset = new Date(parsedBody.sys.sunset*1000)
     var sunriseString = sunrise.toString()
     var sunsetString = sunset.toString()
-    // sunriseString.split("T")
 
-    // console.log(sunrise)
-    // console.log(sunset)
-    // console.log(date)
-// console.log(sunriseString)
-// console.log(sunsetString)
 
     oledExp.setCursor(0, 0)
     oledExp.write("Current weather:")
@@ -45,12 +39,6 @@ https.get(queryURL, (resp) => {
     oledExp.write("Sunrise: " + sunriseString)
     oledExp.setCursor(5, 0)
     oledExp.write("Sunset: " + sunsetString)
-
-
-    
-  //var firstMessage = "Current weather: " + parsedBody.weather[0].description
- // console.log(firstMessage)
- // console.log(firstMessage.length)
 });
  
 }).on("error", (err) => {
